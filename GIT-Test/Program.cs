e5d8d2fe5c5d0f19e579b1c0f209b4fdc210aab8
SpicyCarry <736115@student.inholland.nl>
2025-11-18 20:27:27 +0100

    Hey there!

--- GIT-Test/Program.cs
+++ GIT-Test/Program.cs
@@ -4,14 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-
-        int a = 11;
-
-        for (int i = 1; i < a ; i++)
-        {
-            Console.WriteLine("Hello from Branch Nikita!");
-            Console.WriteLine("This is it.");
-        }
+        Console.WriteLine("Hello from Branch Nikita!");
     }
 }
